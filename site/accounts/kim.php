c6a9ecfb63146cc8ae11119efbaaf648525c9f34
Kim Schneider <kasch.is@gmail.com>
2014-05-23 15:28:52 +0200

    Repo Reset
    - Starterkitkram gelöscht
    - scss init
    - User angelegt

---
+++ site/accounts/kim.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: kim
+email: kasch.is@gmail.com
+password: >
+  $2a$10$rrFlv2/g2QOE2ftv8eX0gu7AiYwGaCLpAhY/Imm7qcDQiL3VNnnx6
+language: de
+token: 0a5973599934b8b9203b297b88b9b4e7c8be2930
