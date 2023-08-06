e9ed4439eae555dc754d6a79cdc993d43263422d
Kim <web@schneider.kim>
2023-08-06 02:16:00 +0200

    prep work (#128)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from "astro/config"
-import sitemap from "@astrojs/sitemap"
+import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://schneider.kim",
-  integrations: [sitemap()],
-})
+	site: 'https://schneider.kim',
+	integrations: [mdx(), sitemap()],
+});
