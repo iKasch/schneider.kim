8fd59659a4746ab3f378759a73a7c2dc4b9de7e3
Kim Schneider <web@schneider.kim>
2023-08-06 02:11:44 +0200

    Revert "🧹cleanup"
    This reverts commit df1258479490323bf46c35f9e645352391be7939.

---
+++ src/components/Card/index.ts
@@ -0,0 +1,6 @@
+import Card from "./Card.astro"
+import CardAvatar from "./CardAvatar.astro"
+import CardContent from "./CardContent.astro"
+import CardTitle from "./CardTitle.astro"
+
+export { Card, CardAvatar, CardContent, CardTitle }
