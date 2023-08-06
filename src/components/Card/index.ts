8a1f70813ae4dc45ee38b28367fcf6d28ce0dc3a
Kim Schneider <web@schneider.kim>
2023-08-06 02:16:49 +0200

    Squashed commit of the following:
    commit e9ed4439eae555dc754d6a79cdc993d43263422d
    Author: Kim <web@schneider.kim>
    Date:   Sun Aug 6 02:16:00 2023 +0200
    
        prep work (#128)

--- src/components/Card/index.ts
+++
@@ -1,6 +0,0 @@
-import Card from "./Card.astro"
-import CardAvatar from "./CardAvatar.astro"
-import CardContent from "./CardContent.astro"
-import CardTitle from "./CardTitle.astro"
-
-export { Card, CardAvatar, CardContent, CardTitle }
